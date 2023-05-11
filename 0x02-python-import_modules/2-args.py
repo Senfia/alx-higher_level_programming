3e274dbef01437f4e4352842c385fc0001e98f61
Senyo Fiawornu <contactsenyo@gmail.com>
2023-05-11 21:51:39 +0000

    final checks

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
-    ls = sys.argv[1:]
-    size = len(ls)
-    print("{:d} argument{}{}".format(size, "" if size <= 1 else "s",
-          "." if not size else ":"))
-    for index, arg in enumerate(ls):
-        print("{:d}: {}".format(index + 1, arg))
+    import sys
+    ls = len(sys.argv) - 1
+    print("{:d} argument{:s}{:s}".format(ls, "s" if ls > 1 else "", "."
+                                         if ls < 1 else ":"))
+    for index, arg in enumerate(sys.argv[1:]):
+        print("{:d}: {:s}".format(index + 1, arg))
