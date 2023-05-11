15efb524ef86c2b9a61c6f242068905b67705119
Senyo Fiawornu <contactsenyo@gmail.com>
2023-05-11 22:08:29 +0000

    final checks

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-
 def print_arg(argv):
     num = len(argv) - 1
     if num == 0:
@@ -16,4 +13,6 @@ def print_arg(argv):
         while i <= num:
             print("{:d}: {:s}".format(li, argv[li]))
             li += 1
-print_arg(sys.argv)
+if __name__ == "__main__":
+    import sys
+    print_arg(sys.argv)
