61bd41e5d6498c5fa450e3bfbffd7455aadde34a
Senyo Fiawornu <contactsenyo@gmail.com>
2023-05-11 06:04:05 +0000

    correction

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def magic_calculation(i, j, k):
-    if (i < j):
-        return c
-    if (j < k):
-        return (i + j)
-    return (a * b - c)
+def remove_char_at(str, n):
+    if (num >= 0):
+        str = str[:num] + str[num + 1:]
+    return (str)
