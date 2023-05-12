e8056bd0b8461a247c3d7cf4561e72d8dedb5045
Senyo Fiawornu <contactsenyo@gmail.com>
2023-05-12 06:01:05 +0000

    commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    args = len(sys.argv) - 1
-    if (args == 0):
-        print("{:d} arguments.".format(args))
-    elif (args == 1):
-        print("{:d} argument:".format(args))
-    else:
-        print("{:d} arguments:".format(args))
-    for str in range(len(sys.argv)):
-        if (str == 0):
-            continue
-        print("{:d}: {:s}".format(str, sys.argv[str]))
+    ls = len(sys.argv) - 1
+    print("{:d} argument{:s}{:s}".format(ls, "s" if ls > 1 else "", "."
+                                         if ls < 1 else ":"))
+    for index, arg in enumerate(sys.argv[1:]):
+        print("{:d}: {:s}".format(index + 1, arg))
