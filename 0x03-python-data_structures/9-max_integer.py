c3682bcbff74aeabbe487edf296deccebee86f24
Senyo Fiawornu <contactsenyo@gmail.com>
2023-05-15 22:25:51 +0000

    Max int

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    list_len = len(my_list)
+    if (list_len < 1):
+        return (None)
+    max_val = my_list[0]
+    for n in my_list:
+        if (n > max_val):
+            max_val = n
+    return(max_val)
