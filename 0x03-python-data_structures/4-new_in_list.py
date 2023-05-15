5c0d1faa3ee99f9320ce48af713f1f8c2a42b280
Senyo Fiawornu <contactsenyo@gmail.com>
2023-05-15 21:54:18 +0000

    added copy func

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,4 +4,4 @@ def new_in_list(my_list, idx, element):
     current_list = my_list.copy()
     if not (idx > len(current_list) - 1 or idx < 0):
         current_list[idx] = element
-    return current_list
+    return (current_list)
