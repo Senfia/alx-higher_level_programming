7a06fd37906f93f5c5fc8f26118024dd729ba236
Senyo Fiawornu <contactsenyo@gmail.com>
2023-05-16 12:33:22 +0000

    removed comments

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,30 +1,4 @@
 #!/usr/bin/python3
-"""
-if __name__ == "__main__":
-    from sys import argv
-    from calculator_1 import add, sub, mul, div
-
-    if len(argv) != 4:
-        print("./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-
-    a, b = int(argv[1]), int(argv[3])
-    math_fns = [add(a, b), sub(a, b), mul(a, b), div(a, b)]
-    math_symbols = ['+', '-', '*', '/']
-
-    if argv[2] not in math_symbols:
-        print("Unknown operator. Only: +, -, * and / available")
-        exit(1)
-    if argv[2] == "+":
-        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    if argv[2] == "-":
-        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    if argv[2] == "*":
-        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    if argv[2] == "/":
-        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-"""
-
 if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
