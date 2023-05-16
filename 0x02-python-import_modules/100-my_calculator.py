8529b634fa3fb2abe10ff2e8267c9df980ceb6a3
Senyo Fiawornu <contactsenyo@gmail.com>
2023-05-16 12:30:04 +0000

    calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+
+    if len(argv) != 4:
+        print("./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a, b = int(argv[1]), int(argv[3])
+    math_fns = [add(a, b), sub(a, b), mul(a, b), div(a, b)]
+    math_symbols = ['+', '-', '*', '/']
+
+    if argv[2] not in math_symbols:
+        print("Unknown operator. Only: +, -, * and / available")
+        exit(1)
+    if argv[2] == "+":
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    if argv[2] == "-":
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    if argv[2] == "*":
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    if argv[2] == "/":
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+
+    if len(argv) != 4:
+        print("./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a, b = int(argv[1]), int(argv[3])
+    math_symbols = ['+', '-', '*', '/']
+
+    if argv[2] not in math_symbols:
+        print("Unknown operator. Only: +, -, * and / available")
+        exit(1)
+
+    operators = {
+        '+': add,
+        '-': sub,
+        '*': mul,
+        '/': div
+    }
+
+    operator = argv[2]
+    result = operators[operator](a, b)
+
+    print("{:d} {} {:d} = {:d}".format(a, operator, b, result))
+
