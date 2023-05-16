382b2623aaf9f6cafd37cd2b751a1ba7241c81bc
Senyo Fiawornu <contactsenyo@gmail.com>
2023-05-16 09:59:34 +0000

    fixed betty error

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -8,18 +8,18 @@
 **/
 void print_python_list_info(PyObject *p)
 {
-    int i = 0;
-    int size = (int)Py_SIZE(p);
-    PyListObject *list = (PyListObject *)p;
+	int i = 0;
+	int size = (int)Py_SIZE(p);
+	PyListObject *list = (PyListObject *)p;
 
-    printf("[*] Size of the Python List = %d\n", size);
-    printf("[*] Allocated = %d\n", list->allocated);
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", list->allocated);
 
-    while (i < size)
-    {
-        PyObject *item = PyList_GetItem(p, i);
-        printf("Element %d: %s\n", i, Py_TYPE(item)->tp_name);
-        i++;
-    }
+	while (i < size)
+	{
+		PyObject *item = PyList_GetItem(p, i);
+
+		printf("Element %d: %s\n", i, Py_TYPE(item)->tp_name);
+			i++;
+	}
 }
-s
