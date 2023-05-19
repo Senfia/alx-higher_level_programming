af80a6a9c9db32887c558e3ff018fbd9c31d897d
Senyo Fiawornu <contactsenyo@gmail.com>
2023-05-19 20:48:22 +0000

    one more correction

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix is None:
-        return (None)
-    if matrix == []:
-        return ([])
-    matrix_sq = [[i*i for i in h-line] for h-line in matrix]
-    return (matrix_sq)
+    matrix_sq = []
+    if matrix:
+        for x in matrix:
+            coln = []
+            for y in x:
+                coln.append(y * y)
+            matrix_sq.append(coln)
+    return matrix_sq
