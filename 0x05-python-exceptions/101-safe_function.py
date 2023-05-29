6a43668b64423565f553ec746c04a7b2091aa95d
Senyo Fiawornu <contactsenyo@gmail.com>
2023-05-29 22:33:51 +0000

    changed variables

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    val = None
+    try:
+        val = fct(*args)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+    finally:
+        return (val)
