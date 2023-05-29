54f216a6539ec5a4a5dbcb879542934a18c97f2c
Senyo Fiawornu <contactsenyo@gmail.com>
2023-05-29 22:37:53 +0000

    changed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    a = 0
-    while (a < x):
+    c = 0
+    while c < x:
         try:
-            print("{}".format(my_list[i]), end="")
-        except:
-            print("")
-            return a
-        a += 1
+            print("{}".format(my_list[c]), end="")
+        except (IndexError, TypeError):
+            break
+        c += 1
     print("")
-    return (a)
+    return (c)
