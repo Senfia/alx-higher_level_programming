3e3fe7e16c9be696bfe48e169bc4230f664800eb
Senyo Fiawornu <contactsenyo@gmail.com>
2023-05-30 23:35:25 +0000

    change function

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,9 +6,11 @@ Class Square definition module
 
 class Square:
     """define a square."""
+    
     def __init__(self, size=0):
         """
         instantiate object
+        
         Arguments:
         	size(int): square size
         """
