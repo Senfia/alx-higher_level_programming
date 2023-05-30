aaaa2977020b728fddb866c82ef1811cb56f2338
Senyo Fiawornu <contactsenyo@gmail.com>
2023-05-30 23:02:02 +0000

    initantiate object

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Class Square definition module
+"""
+
+
+class Square:
+    """define a square."""
+    def __init__(self, size):
+        """
+        instantiate object
+        """
+        self.__size = size
