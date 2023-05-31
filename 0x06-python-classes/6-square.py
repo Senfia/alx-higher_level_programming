b65f142188c6fb1610546fd1909dc6b1ac737e10
Senyo Fiawornu <contactsenyo@gmail.com>
2023-05-31 00:59:07 +0000

    add properties

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -32,28 +32,25 @@ class Square:
 
     @position.setter
     def position(self, value):
-        """Return area of Square object"""
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
-    
+
     def area(self):
-        """Return area."""
+        """Return area of the square."""
         return (self.__size * self.__size)
 
     def my_print(self):
         """Print square using #."""
-        if self.size == 0:
-            print()
+        if self.__size == 0:
+            print("")
             return
-        for l in range(self.position[1]):
-            print()
-        for i in range(self.size):
-            for k in range(self.position[0]):
-                print(" ", end="")
-            for j in range(self.size):
-                print("#", end="")
-            print()
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
