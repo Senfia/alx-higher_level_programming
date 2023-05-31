1b4159743a9a7b5960e8f1599d752cac084e0e3b
Senyo Fiawornu <contactsenyo@gmail.com>
2023-05-31 01:04:56 +0000

    variable reassigned

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -35,8 +35,8 @@ class Square:
     def position(self, value):
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
+                not all(isinstance(number, int) for number in value) or
+                not all(number >= 0 for number in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
