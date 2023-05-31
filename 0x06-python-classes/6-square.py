b28c3c8c2eaba53bb7f8ec164df3822e44144af8
Senyo Fiawornu <contactsenyo@gmail.com>
2023-05-31 00:42:49 +0000

    coordinates

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Class Square definition module"""
+
+
+class Square:
+    """Define a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Arguments:
+            size (int): The size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """set size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return area."""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Print square using #."""
+        if self.size == 0:
+            print()
+            return
+        for l in range(self.position[1]):
+            print()
+        for i in range(self.size):
+            for k in range(self.position[0]):
+                print(" ", end="")
+            for j in range(self.size):
+                print("#", end="")
+            print()
