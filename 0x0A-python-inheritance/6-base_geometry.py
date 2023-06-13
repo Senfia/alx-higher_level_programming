e283dd3aa63af215aba97074650dca63b8a35c6f
Senyo Fiawornu <contactsenyo@gmail.com>
2023-06-13 01:59:26 +0000

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+An empty BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    Raise exception
+    """
+    def area(self):
+    	""" Not implemented """
+        raise Exception("area() is not implemented")
