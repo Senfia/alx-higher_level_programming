eee32fbe59f5bef341171107b046b8f164006b2e
Senyo Fiawornu <contactsenyo@gmail.com>
2023-06-13 01:20:37 +0000

    fixed requirements

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This is module 2-is_same_class
+
+It only defines one funtion
+"""
+
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    evaluates an object and it's subclass
+
+    Return:
+         if the object is an instance of, or if the object 
+         is an instance of a class that inherited from, 
+         the specified class ; otherwise False.
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
