915a144b6f6f694c87a973b2c388510f723fed12
Senyo Fiawornu <contactsenyo@gmail.com>
2023-06-13 01:05:22 +0000

    fixed requirement errors

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -12,6 +12,7 @@ def is_same_class(obj, a_class):
 
 
     Return:
-        True if the object is exactly an instance of the specified class,otherwise False.
+        True if the object is exactly an instance of the specified class,
+        otherwise False.
     """
     return type(obj) == a_class
