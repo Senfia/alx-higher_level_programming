f2073f006597cb0c0bbd838ba27c5f7611acb0c9
Senyo Fiawornu <contactsenyo@gmail.com>
2023-06-13 02:45:00 +0000

    Sqaure

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This is the Square class. 
+It inherits from the Rectangle class.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    class Square
+    """
+    def __init__(self, size):
+        """
+        Initialize Square object
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        Return area of Square object
+        """
+        return (self.__size * self.__size)
