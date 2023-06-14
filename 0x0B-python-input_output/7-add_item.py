1e0a3f30195c3d7a547502e067c1a8e563b8e941
Senyo Fiawornu <contactsenyo@gmail.com>
2023-06-14 04:58:30 +0000

    fixed typos

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+load_from_json_file module.
+It contains one function.
+"""
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+try:
+    arg = load_from_json_file("add_item.json")
+except:
+    arg = []
+for itm in range(1, len(sys.argv)):
+    arg.append(sys.argv[itm])
+save_to_json_file(arg, "add_item.json")
