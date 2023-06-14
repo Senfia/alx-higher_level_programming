34d6191a10947d752c956e117a5c4929c7eb601d
Senyo Fiawornu <contactsenyo@gmail.com>
2023-06-14 10:05:50 +0000

    initial commit

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+load_from_json_file module.
+It contains one function.
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    inserts a line of text to a file,
+    after each line containing a specific string
+    """
+    with open(filename, mode="r+", encoding="utf-8") as f:
+        lines = f.readlines()
+        i = 0
+        for line in lines:
+            if line.find(search_string) != -1:
+                lines.insert(i + 1, new_string)
+            i += 1
+        f.seek(0)
+        f.write("".join(lines))
