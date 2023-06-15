990a2fd245d7d1f2e02c97beb124ce795905246f
Senyo Fiawornu <contactsenyo@gmail.com>
2023-06-15 01:59:56 +0000

    change func

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,33 +6,26 @@ It contains one function.
 
 
 import sys
-from collections import defaultdict
 
-"""Initialize metrics"""
-total_file_size = 0
-status_code_counts = defaultdict(int)
-
+size = 0
+count = 0
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 try:
-    for i, line in enumerate(sys.stdin, 1):
-        """ Parse input line"""
-        parts = line.strip().split()
-        if len(parts) >= 7:
-            status_code = parts[5]
-            file_size = int(parts[6])
-
-            total_file_size += file_size
-            status_code_counts[status_code] += 1
-
-        if i % 10 == 0:
-            print(f"Total file size: {total_file_size}")
-            for code in sorted(status_code_counts.keys()):
-                count = status_code_counts[code]
-                print(f"{code}: {count}")
-            print()
-
+    for line in sys.stdin:
+        if count % 10 == 0:
+            print("File size: {}".format(size))
+            for key, value in sorted(status_codes.items()):
+                if value != 0:
+                    print("{}: {}".format(key, value))
+        for key in status_codes.keys():
+            if str(key) in line:
+                status_codes[key] = status_codes[key] + 1
+        size = line.rsplit(' ', 1)[1]
+        size += int(size)
+        count += 1
 except KeyboardInterrupt:
-    print(f"Total file size: {total_file_size}")
-    for code in sorted(status_code_counts.keys()):
-        count = status_code_counts[code]
-        print(f"{code}: {count}")
+    print("File size: {}".format(size))
+    for key, value in sorted(status_codes.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
 
