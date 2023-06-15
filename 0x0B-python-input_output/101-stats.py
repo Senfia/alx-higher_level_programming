13b10ab9b2d0823209877d4814abed7ae87d8cda
Senyo Fiawornu <contactsenyo@gmail.com>
2023-06-15 01:53:25 +0000

    change func

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,26 +4,35 @@ Sort_dict module.
 It contains one function.
 """
 
-import sys
 
-def sort_dict(dic):
-    """
-    reads stdin line by line and computes metrics
-    """
-    for i in sorted(dic.keys()):
-        print("{}: {:d}".format(i, dic[i]))
+import sys
+from collections import defaultdict
+
+"""Initialize metrics"""
+total_file_size = 0
+status_code_counts = defaultdict(int)
+
 try:
-    size = 0
-    count = 0
-    status_code = {}
-    for line in sys.stdin:
-        size += int(line.split()[-1])
-        status_code[line.split()[-2]] = status_code.get(line.split()[-2], 0) + 1
-        count += 1
-        if count % 10 == 0:
-            print("File size: {:d}".format(size))
-            sort_dict(status_code)
-except KeyboardInterrupt as e:
-        print("File size: {:d}".format(size))
-        sort_dict(status_code)
-        print(e)
+    for i, line in enumerate(sys.stdin, 1):
+        """ Parse input line"""
+        parts = line.strip().split()
+        if len(parts) >= 7:
+            status_code = parts[5]
+            file_size = int(parts[6])
+
+            total_file_size += file_size
+            status_code_counts[status_code] += 1
+
+        if i % 10 == 0:
+            print(f"Total file size: {total_file_size}")
+            for code in sorted(status_code_counts.keys()):
+                count = status_code_counts[code]
+                print(f"{code}: {count}")
+            print()
+
+except KeyboardInterrupt:
+    print(f"Total file size: {total_file_size}")
+    for code in sorted(status_code_counts.keys()):
+        count = status_code_counts[code]
+        print(f"{code}: {count}")
+
