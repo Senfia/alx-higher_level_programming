103b2f9b5b0095e788cc5df87bab70a4ec35ba82
Senyo Fiawornu <contactsenyo@gmail.com>
2023-06-15 03:13:30 +0000

    fixed typo

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -12,7 +12,7 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """
         Creates Student instance.
-        """    
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
