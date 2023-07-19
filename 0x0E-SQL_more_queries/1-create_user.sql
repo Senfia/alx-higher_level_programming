a05032652f874d98396fcf5f67f433e0f70e4c49
Senyo Fiawornu <contactsenyo@gmail.com>
2023-07-19 22:11:04 +0000

    FLUSH PRIVILAGES

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,8 @@
 -- Create user user_0d_1
 -- This user has all privileges
-CREATE USER IF NOT EXISTS 'user_0d_1'@localhost
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.*
-TO 'user_0d_1'@localhost;
+TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
