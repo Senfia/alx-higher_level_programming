d8ed74502e14b3aa04e8c84f7157e6d971cbf3a5
Senyo Fiawornu <contactsenyo@gmail.com>
2023-07-19 19:03:54 +0000

    Dexter

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,6 @@
  -- selects all genres for Dexter
-SELECT a.`name` FROM `tv_genres` AS a
+SELECT a.`name` 
+FROM `tv_genres` AS a
 INNER JOIN `tv_show_genres` AS s
 ON a.`id` = s.`genre_id`
 INNER JOIN `tv_shows` AS t
