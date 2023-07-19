8ee6173a5e6ba1fe33a96da660e4395249133e49
Senyo Fiawornu <contactsenyo@gmail.com>
2023-07-19 18:13:36 +0000

    edit

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- show privilages for user_0d_1 and user_0d_2
-SHOW GRANTS for user_0d_1@localhost;
-SHOW GRANTS for user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_1'@localhost;
+SHOW GRANTS FOR 'user_0d_2'@localhost;
--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- Create user user_0d_1
 -- This user has all privileges
-
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost' 
-IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@localhost;
