c4f546f59b42dc8b2b9fdb235439572ca8645a17
Senyo Fiawornu <contactsenyo@gmail.com>
2023-07-19 18:51:22 +0000

    rework

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,9 @@
 -- List all genres and how many of times they are referenced
-SELECT tv.genres.name AS genre, 
-COUNT(tv_show_genres.genre_id) 
-AS number_shows FROM tv_show_genres 
-INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id 
-GROUP BY tv_genres.name 
-ORDER BY number_shows DESC;
+
+SELECT tv_genres.`name` AS 'genre', COUNT(tv_show_genres.genre_id) 
+AS 'number_shows'
+FROM tv_show_genres
+INNER JOIN tv_genres on tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.`name`
+ORDER BY 2 DESC
+;
