9c182de002c7e24d2a6927a4f25d721bc86b1458
root <root@DESKTOP-JKHB9QO>
2023-08-15 23:17:15 +0000

    Dictionary

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+const neDict = {};
+
+Object.keys(dict).map(function (key) {
+  if (!Array.isArray(nwDict[dict[key]])) {
+    nwDict[dict[key]] = [];
+  }
+  nwDict[dict[key]].push(key);
+});
+
+console.log(nwDict);
