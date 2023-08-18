70bd645522af3c1c1f1dba233234852fc173e50a
root <root@DESKTOP-JKHB9QO>
2023-08-18 16:18:11 +0000

    edited comment

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 this script displays all values in the states table of hbtn_0e_0_usa
-where name matches the argument, (safe from MySQL injection!)
+where name matches the argument, (safe from MySQL injection)
 """
 
 
