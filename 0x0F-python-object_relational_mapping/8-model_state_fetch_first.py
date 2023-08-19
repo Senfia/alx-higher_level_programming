6aa41199561ac36f42d027a2629df0afeefe3dac
root <root@DESKTOP-JKHB9QO>
2023-08-19 17:33:01 +0000

    return first state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,15 +11,19 @@ from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                    argv[2],
-                                                                    argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), echo=False)
     Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
+
     session = Session()
-    first_state = session.query(State).order_by(State.id).first()
-    if first_state is not None:
-        print("{}: {}".format(first_state.id, first_state.name))
+
+    result = session.query(State.id, State.name).order_by(State.id).first()
+    if result:
+        print("{:d}: {}".format(result[0], result[1]))
     else:
         print("Nothing")
+
     session.close()
+    engine.dispose()
