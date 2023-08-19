d7e20a3e9e1f6fefedeb2dffb44d47b945a1741c
root <root@DESKTOP-JKHB9QO>
2023-08-19 17:15:04 +0000

    new edit

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-this script prints all states from the database hbtn_0e_6_usa using SQLAlchemy
+this script prints all states from the database 
+hbtn_0e_6_usa using SQLAlchemy
 """
 
 
