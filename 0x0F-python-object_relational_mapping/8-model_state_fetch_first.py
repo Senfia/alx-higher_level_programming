91aa79cd86630af7817dc337981d4e397bfcbe76
root <root@DESKTOP-JKHB9QO>
2023-08-19 17:24:34 +0000

    redo

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -4,24 +4,22 @@ this script prints all states from the database
 hbtn_0e_6_usa using SQLAlchemy
 """
 
-
-import sys
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sys import argv
 from model_state import Base, State
+
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: username password database name")
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    first_state = session.query(State).order_by(State.id).first()
+    if first_state is not None:
+        print("{}: {}".format(first_state.id, first_state.name))
     else:
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]))
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        state = session.query(State.id, State.name).first()
-        if state:
-            print("{:d}: {:s}".format(state[0], state[1]))
-        else:
-            print("Nothing")
-        session.close()
-        engine.dispose()
+        print("Nothing")
+    session.close()
