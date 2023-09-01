08fde253497ee85674bf314f72c09307023ed399
root <root@DESKTOP-JKHB9QO>
2023-09-01 21:14:50 +0000

    redo

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""This script that
-   fetches https://intranet.hbtn.io/status.
+"""This script that fetches https://intranet.hbtn.io/status.
 """
-
+import urllib.request
 
 if __name__ == '__main__':
-    import urllib.request
 
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
         content = resp.read()
