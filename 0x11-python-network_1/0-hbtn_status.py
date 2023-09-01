ea12b35dcbf779d1c96ebf1fae086fcbc61c777a
root <root@DESKTOP-JKHB9QO>
2023-09-01 21:22:10 +0000

    new

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,8 @@
 """
 import urllib.request
 
-if __name__ == '__main__':
-
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
-        content = resp.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+        html = response.read()
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content:"
+          " {}".format(type(html), html, html.decode('utf-8')))
