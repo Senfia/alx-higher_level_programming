51e1265c64fa85fb10c93ab59871846175383260
root <root@DESKTOP-JKHB9QO>
2023-09-26 20:13:17 +0000

    change url

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@
 const request = require('request');
 
 const paramiters = {
-  url: 'http://swapi.co/api/films/' + process.argv[2],
+  url: 'https://swapi-api.alx-tools.com/api/films/:id' + process.argv[2],
   method: 'GET'
 };
 request(paramiters, function (error, response, body) {
