2e65be6394584aefb494cd58375369d2291e25fe
root <root@DESKTOP-JKHB9QO>
2023-09-26 20:08:45 +0000

    added comments

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
+
+// reads and prints the content of a file
 fs.readFile(process.argv[2], 'utf8', function (err, contents) {
   if (contents === undefined) {
     console.log(err);
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
+
+// writes a string to a file
 fs.writeFileSync(process.argv[2], process.argv[3]);
