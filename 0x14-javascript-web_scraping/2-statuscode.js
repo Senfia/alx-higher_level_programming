ca1eb113de017e92de712437121c51c530a2653e
root <root@DESKTOP-JKHB9QO>
2023-09-26 20:04:51 +0000

    send request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
-
 // Import the 'request' module
 const request = require('request');
 
-// Define the URL to make a request to (provided as a command-line argument)
-const url = process.argv[2];
-
 // Make a request to the specified URL
-request(url, function(error, response) {
-  if (error === null) {
-    // If there is no error, print the HTTP status code
-    console.log('HTTP Status Code:', response.statusCode);
+request(process.argv[2], function (error, response) {
+  if (error == null) {
+    console.log('code: ' + response.statusCode);
   }
 });
-
